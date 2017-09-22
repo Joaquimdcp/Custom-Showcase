133d82d215cdfc5dbad709589d1706d28d85ad76
Joaquim <joaquimdcp@gmail.com>
2017-09-22 08:02:20 +0200

    Some fix for docker

--- showcase/run.sh
+++ showcase/run.sh
@@ -7,33 +7,6 @@ sleep 10
 python3 manage.py makemigrations api
 python3 manage.py migrate
 
-# Ensure admin exists
-USER="admin"
-PASS="pass"
-MAIL="admin@8wires.com"
-script="
-from api.models import User;
-
-username = '$USER';
-password = '$PASS';
-email    = '$MAIL';
-
-if User.objects.filter(email=email).count()==0:
-    User.objects.create_superuser(username, email, password, first_name='Admin', last_name='Overlord');
-    print('Superuser created.');
-else:
-    print('Superuser creation skipped.');
-"
-printf "$script" | python3 manage.py shell
-
-script="
-from api.search_model import search;
-
-search.create_index( ignore = 400 )
-print('Search index created.');
-"
-printf "$script" | python3 manage.py shell
-
 # Run the server
 python3 manage.py runserver 0:8000
 
